12d3bbdf0d608d3758473f2dc555ae0d0b8278d0
ryan <ryschueck@gmail.com>
2021-12-31 19:44:11 -0500

    created global state with recoil. Starting to get the center playlist up and running

--- components/Center.js
+++ components/Center.js
@@ -1,8 +1,13 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import { ChevronDownIcon } from "@heroicons/react/outline";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { shuffle } from "lodash";
+import { playlistState, playlistIdState } from "../atoms/playlistAtom";
+import { useRecoilState, useRecoilValue } from "recoil";
+import useSpotify from "../hooks/useSpotify";
+import Songs from "./Songs";
 
 const colors = [
     "from-indigo-500",
@@ -16,12 +21,26 @@ const colors = [
 
 function Center() {
     const {data: session } = useSession();
+    const spotifyApi = useSpotify();
     const [color, setColor] = useState(null);
+    const playlistId = useRecoilValue(playlistIdState);
+    const [playlist, setPlaylist] = useRecoilState(playlistState);
+
 
     useEffect(() => {
         setColor(shuffle(colors).pop());
         
-    }, [])
+    }, [playlistId]);
+
+    useEffect(() => {
+        spotifyApi
+        .getPlaylist(playlistId)
+        .then((data) => {
+            setPlaylist(data.body);
+        }).catch((err) => console.log("Something went wrong!", err));
+    }, [spotifyApi, playlistId])
+
+    console.log(playlist)
     return (
         <div className="flex-grow">
             <header className="absolute top-5 right-8">
@@ -35,10 +54,21 @@ function Center() {
                     <ChevronDownIcon className="h-5 w-5" />
                 </div>
             </header>
-            <section className={`flex items-end space-x-7 bg-gradient-to-b 
-            to-black ${color} h-80 text-white p-8`}>
-
+            <section 
+                className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} 
+                h-80 text-white p-8`}>
+                <img 
+                className="h-44 w-44 shadow-2xl" 
+                src={playlist?.images?.[0]?.url} 
+                alt=""/>
+                <div>
+                    <p>PLAYLIST</p>
+                    <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">{playlist?.name}</h1>
+                </div>
             </section>
+            <div>
+                {/* <Songs /> */}
+            </div>
         </div>
     )
 }
